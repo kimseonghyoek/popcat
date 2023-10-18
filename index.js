4be666162b4c4536a0190bd88b8a46144db8f4dc
overloper <overloper0225@gmail.com>
2023-10-18 23:43:17 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  var popit = document.getElementById("contents");
+  popit.click = onClick;
+}
+
+function onClick() {
+  alert("pop");
+};
+
+function counter(count) {
+  
+}
