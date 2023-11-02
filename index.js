023015cf67d6cc0a6807a978e4dbe0885d344b35
overloper <overloper0225@gmail.com>
2023-11-03 06:00:07 +0900

    update

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-var time = document.getElementById('time');
+var time = document.getElementById("time");
 var count = document.getElementById("count");
-var name = document.getElementById('name');
+var name = document.getElementById("name");
 var cat = document.getElementById("contents");
 var num = 0;
 var second = 10;
 
 function onClick() {
-  document.getElementById('cat').src = './img/popcat.png';
-  var audio = new Audio('./pop-cat-original-meme_3ObdYkj.mp3');
-  audio.play(); 
+  document.getElementById("cat").src = "./img/popcat.png";
+  var audio = new Audio("./pop-cat-original-meme_3ObdYkj.mp3");
+  audio.play();
   num = parseInt(count.innerText, 10);
   count.innerText = num + 1;
-};
+}
 
 function onUp() {
-  document.getElementById('cat').src = './img/closecat.png';
-};
+  document.getElementById("cat").src = "./img/closecat.png";
+}
 
-window.onload = function() {
-  alert("Ready?")
+window.onload = function () {
+  alert("Ready?(누르면 바로 시작합니다!!)");
   setInterval(() => {
     second--;
     time.innerText = second;
     if (second == 0) {
-      alert("그만!")
-      alert(num+1);
+      alert("그만!");
+      alert(num + 1);
       location.reload(true);
-    };
+    }
   }, 1000);
-}
+};
