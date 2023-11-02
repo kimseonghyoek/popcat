7e4b890b84ae4899ecd7927113ea019bfa9dc65f
overloper <overloper0225@gmail.com>
2023-11-03 05:59:02 +0900

    update

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+var time = document.getElementById('time');
+var count = document.getElementById("count");
+var name = document.getElementById('name');
+var cat = document.getElementById("contents");
+var num = 0;
+var second = 10;
+
 function onClick() {
   document.getElementById('cat').src = './img/popcat.png';
   var audio = new Audio('./pop-cat-original-meme_3ObdYkj.mp3');
   audio.play(); 
+  num = parseInt(count.innerText, 10);
+  count.innerText = num + 1;
 };
 
 function onUp() {
@@ -9,5 +18,14 @@ function onUp() {
 };
 
 window.onload = function() {
-  var name = prompt("이름을 입력하세요!");
+  alert("Ready?")
+  setInterval(() => {
+    second--;
+    time.innerText = second;
+    if (second == 0) {
+      alert("그만!")
+      alert(num+1);
+      location.reload(true);
+    };
+  }, 1000);
 }
